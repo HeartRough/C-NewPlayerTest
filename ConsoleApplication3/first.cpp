435be50d47366b6d48d18bec15a3b90ddc1364a0
HeartRougn <1809877964@qq.com>
2019-07-28 17:06:41 +0800

    first
    first

--- ConsoleApplication3/first.cpp
+++ ConsoleApplication3/first.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 
 int main() {
-	cout << "移除了一个东东" << endl;
+	//字符串的输入输出会有异常
+	//cout << "移除了一个东东" << endl;
+	/*
+	cout << "Please print 20 end:";
+	char str[20];
+	scanf_s("%s", str);
+	cout << str;
+	*/
+	//while和++的套路
+	/*
+	int n = 0;
+	while (n++ <= 2);
+	cout << n;
+	*/
+	//随机数时间种子
+	/*
+	srand(unsigned(time(NULL)));
+	for(int i=0;i<10;i++){
+		cout << 5 + rand() % 11 << "\t" << endl;
+		cout << random(5, 15) << endl;
+	}
+	*/
+
 }
 
