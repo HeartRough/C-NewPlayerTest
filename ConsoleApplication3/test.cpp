577ae59c301fd4b2ca21dbe4d41f89ff07db669f
HeartRougn <1809877964@qq.com>
2019-08-07 00:23:30 +0800

    ptr

---
+++ ConsoleApplication3/test.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+
+//int main() {
+//	/*
+//	char c = 'c';
+//	char *cptr = &c;
+//	void *voidptr = cptr;
+//	void *voidptr2 = &c;
+//	cout << voidptr2 << "\t" << *voidptr2 << endl;
+//	cout << voidptr << endl;
+//	cout << *cptr << endl;
+//	cout << *voidptr << endl;
+//	//void指向的数据类型未知，直接编译器拒绝访问
+//	//cout << "cptr的输出：" << cptr << endl;
+//	//cout << "转换类型后的输出：" << (void *)cptr << endl;
+//	*/
+//}
