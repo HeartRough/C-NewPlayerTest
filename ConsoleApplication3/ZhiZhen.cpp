a1c0cf3f9749bf923bd416562d49231eba017abe
HeartRougn <1809877964@qq.com>
2019-08-06 23:28:07 +0800

    null/char ptr

--- ConsoleApplication3/ZhiZhen.cpp
+++ ConsoleApplication3/ZhiZhen.cpp
@@ -1,5 +1,22 @@
-#include "pch.h"
+﻿#include "pch.h"
 
 int main() {
+	//空指针,char指针的正确输出方法
+	/*
+	void * voidptr = nullptr;
+	int* a = nullptr;
+	int b = 66;
+	cout << a<<endl;
+	a = &b;
+	cout << a << "\t" << &a<<endl;
+	char c = 'c';
+	char* cptr = &c;
+	//错误
+	cout << cptr << "\t"<<*cptr<<endl;
+	//voidptr = (void *)cptr;
+	//cout << *voidptr;
+	//正确
+	cout << (void *)cptr<<endl;
+	*/
 
 }
